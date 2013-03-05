19130d5a7089cf854861fd98aaba4a8892ef38b3
Markus Schirp <mbj@seonic.net>
2013-03-05 12:18:49 +0100

    Break up fuzzing operations into classes

--- lib/veritas/fuzzer.rb
+++ lib/veritas/fuzzer.rb
@@ -5,7 +5,7 @@ require 'diffy'
 require 'pp'
 
 module Veritas
-  # Fuzzer, to be broken up in much smaller parts
+  # Fuzzer namespace
   class Fuzzer
     include Composition.new(:gateway, :relation)
 
@@ -19,18 +19,30 @@ module Veritas
       new(*args).run
     end
 
+    # Run fuzzer loop
+    #
+    # @reutrn [undefined]
+    #
+    # @api private
+    #
     def run
       loop do
-        run_with_timeout
+        run_stack
       end
     end
 
   private
 
-    def assert_equal_key(left_key, right_key)
-      next if left_key < right_key
-      left, right = relations.values_at(left_key, right_key)
-      next if left == right
+    def equal_keys?(relations, left, right)
+      return true if left < right
+      left, right = relations.values_at(left, right)
+      left == right
+    end
+
+    def assert_equal_key(relations, left_key, right_key)
+      if equal_keys?(relations, left_key, right_key) 
+        return
+      end
    
       left_table  = table(left.sort_by  { left.header  })
       right_table = table(right.sort_by { right.header })
@@ -47,10 +59,11 @@ module Veritas
 
     def assert_equality(relations, relation)
       relations.keys.permutation(2) do |(left_key, right_key)|
+        assert_equal_key(relations, left_key, right_key)
       end
     end
 
-    def run
+    def run_stack
       stack = [ [ gateway, relation ] ]
 
       while element = stack.last
@@ -65,7 +78,7 @@ module Veritas
 
             assert_equality(relations, relation)
 
-            method, args, block = next_operation(gateway, stack.length) if relation.any?
+            method, args, block = Operation.next(gateway, stack.length) if relation.any?
 
             if method.nil?
               stack.pop
@@ -80,68 +93,8 @@ module Veritas
           puts "timeout"
           break
         end
-
-        puts stack.length
-
       end
     end
-
-    def next_operation(relation, level)
-      # TODO: break this up into separate classes for each mutation
-      #       and then randomly select the mutator class.
-      # TODO: add more operations
-      method = [ 
-        :project, 
-        :remove, 
-        :extend, 
-        :join, 
-        :product, 
-    #   :union, 
-    #   :intersect, 
-    #   :difference, 
-        :restrict, 
-      ].sample
-
-      header      = relation.header
-      header_size = header.to_a.size
-      attributes  = header.to_a.sample(rand(header_size))
-
-      case method
-      when nil
-        nil  # do nothing
-      when :extend
-        attribute = attributes.sample
-        return unless attribute.kind_of?(Veritas::Attribute::Integer)
-        operator = [ :*, :%, :+, :-].sample
-        if operator == :%
-          operand = [1, 2, -1].sample
-        else
-          operand = [1, 2, 0, -1].sample
-        end
-        [ :extend, [], proc { |r| r.add(:"#{attribute.name}_#{level}", r.send(attribute.name).send(operator, operand)) } ]
-      when :base
-        [ :restrict, [], proc { Veritas::Function::Proposition::Tautology.new } ]
-      when :project, :remove
-        [ method, [ attributes ] ] if attributes.any?
-      when :restrict
-        predicate = attributes.reduce(Veritas::Function::Proposition::Tautology.new) do |predicate, attribute|
-          function = [ :eq, :ne, :gt, :gte, :lt, :lte ].sample
-          value    = relation.map { |tuple| tuple[attribute] }.sample
-     
-          predicate.and(attribute.send(function, value))
-        end
-     
-        [ :restrict, [], proc { predicate } ]
-      when :join, :product, :union, :intersect, :difference
-        other_method, other_args, other_block = next_operation(relation, level)
-        return unless other_method
-        right = relation.send(other_method, *other_args, &other_block) 
-        [ :join, [ right ] ]
-      else
-        raise "unhandled operation: #{method}"
-      end
-    end
- 
 
     def table(relation)
       Terminal::Table.new do |table|
@@ -149,5 +102,8 @@ module Veritas
         table.rows     = relation.map(&:to_ary)
       end
     end
+
   end
 end
+
+require 'veritas/fuzzer/operation'
---
+++ lib/veritas/fuzzer/operation.rb
@@ -0,0 +1,96 @@
+module Veritas
+  class Fuzzer
+    # Abstract base class for fuzzing operations
+    class Operation
+      include AbstractType, Adamantium::Flat, Composition.new(:relation, :level)
+
+      def self.next(*args)
+        fuzzer = FUZZERS.sample
+        fuzzer.new(*args).operation
+      end
+      
+      def attributes
+        relation.header.to_a
+      end
+      memoize :attributes
+
+      def random_attributes
+        attributes.sample(rand(attributes.size))
+      end
+      memoize :random_attributes
+
+      def random_attribute
+        attributes.sample
+      end
+      memoize :random_attribute
+
+      abstract_method :operation
+
+      class Noop < self
+        def operation; end
+      end
+
+      class Extend < self
+        def operation
+          attribute = random_attribute
+          return unless attribute.kind_of?(Veritas::Attribute::Integer)
+          operator = [ :*, :%, :+, :-].sample
+          if operator == :%
+            operand = [1, 2, -1].sample
+          else
+            operand = [1, 2, 0, -1].sample
+          end
+          [ :extend, [], proc { |r| r.add(:"#{attribute.name}_#{level}", r.send(attribute.name).send(operator, operand)) } ]
+        end
+      end
+
+      class Project < self
+
+        def method
+          [ :project, :remove ].sample
+        end
+
+        def operation
+          attributes = random_attributes
+          [ method, [ attributes ] ] if attributes.any?
+        end
+
+      end
+
+      class Restriction < self
+        def operation
+          predicate = attributes.reduce(Veritas::Function::Proposition::Tautology.new) do |predicate, attribute|
+            function = [ :eq, :ne, :gt, :gte, :lt, :lte ].sample
+            value    = relation.map { |tuple| tuple[attribute] }.sample
+            predicate.and(attribute.send(function, value))
+          end
+       
+          [ :restrict, [], proc { predicate } ]
+        end
+      end
+
+      class Binary < self
+
+        def method
+          [ :join, :product, :union, :intersect, :difference ].sample
+        end
+
+        def operation
+          other_method, other_args, other_block = Operation.next(relation, level)
+          return unless other_method
+          right = relation.send(other_method, *other_args, &other_block) 
+          [ :join, [ right ] ]
+        end
+
+      end
+      
+      FUZZERS = [
+        Noop,
+        Extend,
+        Project,
+        Restriction,
+        Binary
+      ]
+    end
+  end
+end
